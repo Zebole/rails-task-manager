fc2aabca55d06bb9864633bd114bf0fac7964fa1
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-07-27 16:20:46 +0400

    Updates a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,23 @@ class TasksController < ApplicationController
   end
 
   def new
-
+    @task = Task.new
   end
 
   def create
     task = Task.new(task_params)
     task.save!
-    redirect_to task_path(task.id)
+    redirect_to task_path(@task.id)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task.id)
   end
 
   private
