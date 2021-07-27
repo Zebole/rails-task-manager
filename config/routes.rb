f5fceca67e40f6d547aea9a0aa900ab95060b325
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-07-27 14:27:13 +0400

    Adds Show page for Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks', to: 'tasks#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks',     to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show',  as: :task
 end
